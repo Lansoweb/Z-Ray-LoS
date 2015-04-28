79d603abdf1ba70d780bc398c29690797e098532
Leandro Silva <leandro@leandrosilva.info>
2015-04-28 12:30:09 -0300

    Added LosLicense and LosDomain

--- zray.php
+++ zray.php
@@ -11,6 +11,37 @@ class LosZray
         $date = date("Y-m-d H:i:s", $sec).substr($usec, 1);
         $storage['LosLog'][] = array('date' => $date, 'message' => $msg);
     }
+
+    public function storeDomain($context, &$storage)
+    {
+        $domainService = $context["returnValue"];
+        $storage['LosDomain'][] = [
+            $domainService->getDomain() => [
+                'domain' => $domainService->getDomain(),
+                'layout' => $domainService->getLayout(),
+                'config' => $domainService->getDomainOptions()
+            ],
+        ];
+    }
+
+    public function storeLicense($context, &$storage)
+    {
+        $license = $context["returnValue"];
+        $storage['LosLicense'][] = [
+            'license' => $license
+        ];
+    }
+
+    public function storeLicenseValidator($context, &$storage)
+    {
+        $validator = $context["returnValue"];
+        $storage['LosLicense'] = [
+            [
+                'isLicensed' => $validator->isLicensed(),
+                'license' => $validator->getServiceLocator()->get('loslicense.license')
+            ]
+        ];
+    }
 }
 
 $losStorage = new \Los\LosZray();
@@ -20,3 +51,6 @@ $loslog->setMetadata(array(
 ));
 $loslog->setEnabledAfter('Zend\Mvc\Application::init');
 $loslog->traceFunction("LosLog\\Log\\StaticLogger::save",  array($losStorage, 'storeLog'), function () {});
+$loslog->traceFunction("LosDomain\\Service\\Domain::setServiceLocator", function () {}, array($losStorage, 'storeDomain'));
+//$loslog->traceFunction("LosLicense\\Options\\ModuleOptions::getLicense", function () {}, array($losStorage, 'storeLicense'));
+$loslog->traceFunction("LosLicense\\Service\\ValidatorService::setServiceLocator", function () {}, array($losStorage, 'storeLicenseValidator'));
